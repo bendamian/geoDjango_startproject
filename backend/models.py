fc23e0ab733465d3496302778544066ac19ce65a
bendamian <badamian@gmail.com>
2024-02-12 12:50:56 +0000

    Create a test model in backend molds.py

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+# test model
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
