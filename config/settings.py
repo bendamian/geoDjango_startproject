cd7077654b73fe542f1d99b13b4f4569d8ab87f6
bendamian <badamian@gmail.com>
2024-02-12 16:29:54 +0000

    Activating GeoDjango by installing GDAL,SpatiaLite

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'backend',
     'frontend'
 ]
@@ -67,7 +68,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
