45dd5c86beb72c7b5c8249cff98617abc3e61120
bendamian <badamian@gmail.com>
2024-02-26 18:32:07 +0000

    Merge remote-tracking branch 'origin/main'

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
 
     path('area_form/', views.create_area, name='area_form'),
-    path('marker_map/', views.create_marker, name='marker_map'),
+    path('marker_map', views.create_marker, name='marker_map'),
+    path('map_claster/', views.create_marker_cluster, name='map_claster'),
+    path('polyline/',views.create_polyline, name='polyline'),
+    path('world/',views.create_world,name='world')
 
 ]
